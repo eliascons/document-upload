007542e33dac665efc4fa1ba61ae9ace04d24d8f
Elias Consalvo <eliasconsalvo@gmail.com>
2024-02-19 22:31:08 -0500

    add modal, and header

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,54 @@
-import './App.css'
+import { useCallback, useState } from "react";
+import "./App.css";
+import Button from "@mui/material/Button";
+import Modal from "@mui/material/Modal";
+import Header from "./components/Header";
+import Box from "@mui/material/Box";
 
-function App() {
+const openModalBtnStyles = {
+  textTransform: "none",
+  borderRadius: "10px",
+  backgroundColor: "#243D6B",
+  width: "10rem",
+};
 
+const modalBodyStyles = {
+  display: "flex",
+  backgroundColor: "white",
+  width: "100%",
+  height: "100%",
+  margin: "auto",
+  borderRadius: "10px",
+  flexDirection: "column",
+};
+
+const mainContainer = {};
+
+const App = () => {
+  const [showModal, setShowmodal] = useState(false);
+
+  const handleOpenModal = useCallback(() => {
+    setShowmodal(!showModal);
+  }, [showModal]);
   return (
-    <>
-      <div>Initial commit</div>
-    </>
-  )
-}
+    <Box className="app">
+      <Button
+        variant="contained"
+        color="primary"
+        style={openModalBtnStyles}
+        onClick={handleOpenModal}
+      >
+        Open Modal
+      </Button>
+      <Modal open={showModal} className="modal-content">
+        <Box style={modalBodyStyles}>
+          <Box style={mainContainer}>
+            <Header handleOpenModal={setShowmodal} />
+          </Box>
+        </Box>
+      </Modal>
+    </Box>
+  );
+};
 
-export default App
+export default App;
---
+++ src/components/Header.jsx
@@ -0,0 +1,64 @@
+import Box from "@mui/material/Box";
+import CloseIcon from "@mui/icons-material/Close";
+import Button from "@mui/material/Button";
+import { useCallback } from "react";
+
+const headerStyles = {
+  display: "flex",
+  justifyContent: "center",
+  fontWeight: 600,
+  fontSize: "2rem",
+  fontFamily: "Roboto",
+  color: "#243D6B",
+  paddingBottom: "1rem",
+  borderBottom: "solid 2px #f5eaea",
+  width: "20rem",
+};
+
+const closeModalBtnStyles = {
+  width: "2rem",
+  textTransform: "none",
+  borderRadius: "8px",
+  height: "1.5rem",
+  margin: "1rem 0 .5rem 2rem",
+  padding: "0",
+  flexDirection: "column",
+  alignItems: "flex-end",
+  backgroundColor: "#243D6B",
+  minWidth: "2rem",
+};
+
+const innerBoxStyles = {
+  display: "flex",
+  alignItems: "center",
+  flexDirection: "column",
+};
+
+// eslint-disable-next-line react/prop-types
+const Header = ({ handleOpenModal }) => {
+  const handleClose = useCallback(() => {
+    handleOpenModal(false);
+  }, [handleOpenModal]);
+  return (
+    <Box
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        flexDirection: "column",
+      }}
+    >
+      <Button
+        variant="contained"
+        color="primary"
+        onClick={handleClose}
+        sx={closeModalBtnStyles}
+        startIcon={<CloseIcon />}
+      />
+      <Box sx={innerBoxStyles}>
+        <Box sx={headerStyles}>Document Upload</Box>
+      </Box>
+    </Box>
+  );
+};
+
+export default Header;
